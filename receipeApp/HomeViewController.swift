88a2767b09fc2ab4e0465de05b1850e638f4f962
Zeeshan <ezeeshanpk@gmail.com>
2022-06-18 10:41:41 +0400

    Renamed to HomeViewController

--- receipeApp/HomeViewController.swift
+++ receipeApp/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  receipeApp
 //
 //  Created by Muhammad Zeeshan on 18/06/2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
